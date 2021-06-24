a14dbaceaa7a349646d66f07b86011c0266aa87c
José H. Dall Asta <nekitbr3131@gmail.com>
2021-06-24 05:10:14 -0300

    Minor improvements

--- game.py
+++ game.py
@@ -31,7 +31,7 @@ def dead(score):
     pygame.mixer.music.stop()
     message_display("Você Morreu com "+str(score-1)+" pontos!")
 
-def escrevendoPlacar(score, vidas):
+def placar(score, vidas):
     font = pygame.font.SysFont(None, 25)
     texto = font.render("score:"+str(score), True, branco)
     texto2 = font.render("vidas:"+str(vidas), True, branco)
@@ -51,10 +51,10 @@ def jogo():
     # [ini] image assets
     fundo = pygame.image.load("assets/images/background.jpg")
     fundo = pygame.transform.scale(fundo, (800, 600))
-    food = [['trash0.png', 'trash1.png', 'trash2.png', 'trash3.png', 'trash4.png', 'trash5.png'], ['fruit0.png', 'fruit1.png', 'fruit2.png', 'fruit3.png', 'fruit4.png', 'fruit5.png']]
+    comida_lista = [['trash0.png', 'trash1.png', 'trash2.png', 'trash3.png', 'trash4.png', 'trash5.png'], ['fruit0.png', 'fruit1.png', 'fruit2.png', 'fruit3.png', 'fruit4.png', 'fruit5.png']]
     comidaType = random.randint(0, 1) # pega tipo da comida trash/candy
     comidaID = random.randint(0, 5) # pega index do tipo, 0 a 5
-    comida = pygame.image.load('assets/images/'+food[comidaType][comidaID])
+    comida = pygame.image.load('assets/images/'+comida_lista[comidaType][comidaID])
     comida = pygame.transform.scale(comida, (120, 120))
     yoshi = (pygame.image.load("assets/images/yoshi-still.png"))
     yoshi = pygame.transform.scale(yoshi, (120, 120))
@@ -62,13 +62,13 @@ def jogo():
 
     playerPosicaoX = largura * 0.45
     playerPosicaoY = altura * 0.8
-    ironLargura = 120
+    yoshiLargura = 120
     movimentoX = 0
     comidaX = largura * 0.45
     comidaY = -220
     comidaLargura = 120
     comidaAltura = 120
-    missilVelocidade = 5
+    comidaVelocidade = 5
     vidas = 3
     score = 1
     anti_loop = False
@@ -87,14 +87,14 @@ def jogo():
                 if evento.key == pygame.K_LEFT:
                     yoshi = (pygame.image.load("assets/images/yoshi-running-left.gif"))
                     yoshi = pygame.transform.scale(yoshi, (120, 120))
-                    if missilVelocidade < 10:
-                        movimentoX = -10*missilVelocidade/5*0.1-10
+                    if comidaVelocidade < 10:
+                        movimentoX = -10*comidaVelocidade/5*0.1-10
                     else:
                         movimentoX = -12
                 elif evento.key == pygame.K_RIGHT:
                     yoshi = (pygame.image.load("assets/images/yoshi-running-right.gif"))
                     yoshi = pygame.transform.scale(yoshi, (120, 120))
-                    if missilVelocidade < 10:
+                    if comidaVelocidade < 10:
                         movimentoX = 10*score*0.1+10
                     else:
                         movimentoX = 12
@@ -113,22 +113,22 @@ def jogo():
 
         display.blit(yoshi, (playerPosicaoX, playerPosicaoY))
         display.blit(comida, (comidaX, comidaY))
-        comidaY += missilVelocidade
+        comidaY += comidaVelocidade
 
         # [ini] colisão com player, reinicia posição
-        if (comidaY + comidaAltura >= playerPosicaoY) and (((comidaX > playerPosicaoX) and (playerPosicaoX+ironLargura > comidaX)) or ((comidaX+comidaLargura > playerPosicaoX) and (comidaX+comidaLargura < playerPosicaoX+ironLargura))):
+        if (comidaY + comidaAltura >= playerPosicaoY) and (((comidaX > playerPosicaoX) and (playerPosicaoX+yoshiLargura > comidaX)) or ((comidaX+comidaLargura > playerPosicaoX) and (comidaX+comidaLargura < playerPosicaoX+yoshiLargura))):
             comidaY = -220
             comidaX = random.randrange(0, largura-50) # reseta comida
-            if missilVelocidade < 10:
-                missilVelocidade += 0.9
+            if comidaVelocidade < 10:
+                comidaVelocidade += 0.9
 
-            if food[comidaType][comidaID] in food[1]:
+            if comida_lista[comidaType][comidaID] in comida_lista[1]:
                 score += 1
                 pygame.mixer.Sound.play(pygame.mixer.Sound('assets/sounds/'+sounds_act[random.randint(0, 3)]))
-            elif food[comidaType][comidaID] in food[0]:
+            elif comida_lista[comidaType][comidaID] in comida_lista[0]:
                 vidas -= 1
                 if vidas == 0:
-                    escrevendoPlacar(score, vidas)
+                    placar(score, vidas)
                     pygame.display.update()
                     pygame.mixer.Sound.play(pygame.mixer.Sound('assets/sounds/'+sounds_stats[1]))
                     dead(score)
@@ -139,12 +139,12 @@ def jogo():
 
             comidaType = random.randint(0, 1) # pega tipo da comida trash/candy
             comidaID = random.randint(0, 5) # pega index do tipo, 0 a 5
-            comida = pygame.image.load('assets/images/'+food[comidaType][comidaID])
+            comida = pygame.image.load('assets/images/'+comida_lista[comidaType][comidaID])
             comida = pygame.transform.scale(comida, (120, 120))
         # [fim] colisão com player, reinicia posição
 
 
-        escrevendoPlacar(score, vidas)
+        placar(score, vidas)
 
 
         # [ini] colisão:
@@ -152,15 +152,15 @@ def jogo():
             anti_loop = False
 
         if comidaY > altura+comidaAltura/2 and anti_loop == False:
-            if missilVelocidade < 10:
-                missilVelocidade += 0.9 # aumenta velocidade do obj
+            if comidaVelocidade < 10:
+                comidaVelocidade += 0.9 # aumenta velocidade do obj
 
-            if food[comidaType][comidaID] in food[1]:
+            if comida_lista[comidaType][comidaID] in comida_lista[1]:
                 vidas -= 1
                 pygame.mixer.Sound.play(pygame.mixer.Sound('assets/sounds/'+sounds_stats[0]))
                 
             if vidas == 0:
-                escrevendoPlacar(score, vidas)
+                placar(score, vidas)
                 pygame.display.update()
                 pygame.mixer.Sound.play(pygame.mixer.Sound('assets/sounds/'+sounds_stats[1]))
                 dead(score)
@@ -173,7 +173,7 @@ def jogo():
 
             comidaType = random.randint(0, 1) # pega tipo da comida trash/candy
             comidaID = random.randint(0, 5) # pega index do tipo, 0 a 5
-            comida = pygame.image.load('assets/images/'+food[comidaType][comidaID]) # reseta comida e seu tipo
+            comida = pygame.image.load('assets/images/'+comida_lista[comidaType][comidaID]) # reseta comida e seu tipo
             comida = pygame.transform.scale(comida, (120, 120)) # reseta comida e seu tipo
         # [fim] colisão:
 
